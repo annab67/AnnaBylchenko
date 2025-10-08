fa7b4937c7e6a33a185a8740b60bc1a5b049c4b4
Anna Bylchenko <a.bylchenko@gmail.com>
2025-10-08 20:25:50 +0500

    Задание 5 (Вариант 3)

---
+++ task05/task05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = Calculate(2, 3, 4, 5);
+            Console.WriteLine(Math.Round(x, 3));
+            
+        }
+        static double Calculate(double a, double b, double c, double d) =>
+                Math.Sqrt(a + Math.Sqrt(b + Math.Sqrt(c + Math.Sqrt(d))));
+    }
+}
