ab739859407b7cbad14444ebc10226921ead6102
Anna Bylchenko <a.bylchenko@gmail.com>
2025-10-22 19:52:28 +0500

    Задание 8.1 (Вариант 3)

---
+++ task08.1/task08.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task08._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число n");
+            var n = int.Parse(Console.ReadLine());
+
+            if (IfLogicalExpressionTrue(n))
+                Console.WriteLine("Число n кратно 5, но не кратно 7");
+            else
+                Console.WriteLine("Либо число n не кратно 5, либо кратно 7");
+        }
+
+        static bool IfLogicalExpressionTrue(int n) =>
+            (n % 5 == 0) && (n % 7 != 0);
+    }
+}
