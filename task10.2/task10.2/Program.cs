1df81775924c92760497fcc36012aab57814e6b2
Anna Bylchenko <a.bylchenko@gmail.com>
2025-11-16 15:15:53 +0500

    Задание 10.2

---
+++ task10.2/task10.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество чисел");
+            var n = int.Parse(Console.ReadLine());
+
+            double product = 1;
+
+            for (int i = 0; i < n; i++) 
+            {
+                Console.WriteLine($"Введите {i + 1}-е число");
+                product *= int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Произведение = {product}");
+        }
+    }
+}
